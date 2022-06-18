7ca3c0f1ee721f842d0e2ff95000297040605cab
hchier <1464002726@qq.com>
2022-06-18 20:40:36 +0800

    1st

---
+++ src/main/java/xyz/hchier/hzone/HzoneApplication.java
@@ -0,0 +1,13 @@
+package xyz.hchier.hzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/hchier/hzone/HzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.hchier.hzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
