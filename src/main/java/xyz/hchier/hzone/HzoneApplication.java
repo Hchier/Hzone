33b5c49c1d73f706f80aa004c4b8b9d9f511c58f
hchier <1464002726@qq.com>
2022-06-18 21:28:55 +0800

    env

--- src/main/java/xyz/hchier/hzone/HzoneApplication.java
+++ src/main/java/xyz/hchier/hzone/HzoneApplication.java
@@ -3,6 +3,9 @@ package xyz.hchier.hzone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Hchier
+ */
 @SpringBootApplication
 public class HzoneApplication {
 
